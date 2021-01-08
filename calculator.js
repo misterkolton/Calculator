7047f85097eb8a8560487b6350d044ed12daa09d
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-01-08 15:45:06 -0700

    color modes are working

--- calculator.js
+++ calculator.js
@@ -4,3 +4,24 @@
 // const equalsButton = document.querySelector(['data-equals'])
 // const deleteButton = document.querySelector(['data-delete'])
 // console.log(deleteButton)
+
+function selectMode(mode) {
+
+var color = "#72bab9"
+
+if (mode === "dark-mode") {
+    color = "#333333";
+    document.getElementById("theme").style
+    .backgroundColor = color;  
+} else if (mode === "light-mode") {
+    color = "#ffffff";
+} else if (mode === "color-mode") {
+    color = "#72bab9";
+} else { 
+    color = "color-mode";
+}
+
+document.getElementById("theme").style
+.backgroundColor = color;
+
+}
