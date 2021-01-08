52311d9a8859b11187b3fc4b509b5d94946464ff
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-01-07 21:59:21 -0700

    this is the first draft of the calculator

---
+++ calculator.js
@@ -0,0 +1,6 @@
+// const numberButtons = document.querySelectorAll('[data-number]')
+// const operationsButtons = document.querySelectorAll('[data-operation]')
+// const clearButton = document.querySelector('[data-all-clear]')
+// const equalsButton = document.querySelector(['data-equals'])
+// const deleteButton = document.querySelector(['data-delete'])
+// console.log(deleteButton)
